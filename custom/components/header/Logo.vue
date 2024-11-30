a65d29240e4b15b4b62719422f6e2d2c6383deda
Jonathan Martz <git@jonathan-martz.de>
2024-11-30 14:31:32 +0100

    link logo

--- custom/components/header/Logo.vue
+++ custom/components/header/Logo.vue
@@ -1,3 +1,5 @@
 <template>
-    <img src="/3d-world-logo.svg" class="h-24 px-3 py-3" alt="">
+    <a href="/">
+        <img src="/3d-world-logo.svg" class="h-24 px-3 py-3" alt="">
+    </a>
 </template>
