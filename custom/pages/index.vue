574feee1d805cc037026466ec915d25ae9603922
Jonathan Martz <git@jonathan-martz.de>
2024-11-30 17:22:48 +0100

    add homepage hero to index

--- custom/pages/index.vue
+++ custom/pages/index.vue
@@ -7,6 +7,7 @@ storeBreadcrumb.clear();</script>
 <template>
     <section class="page">
         <div class="mx-auto max-w-6xl grid grid-cols-6 gap-3 bg-white rounded-lg">
+            <HomepageHero />
             <CatalogProductGrid category-name="Welcome" />
         </div>
     </section>
